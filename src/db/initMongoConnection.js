1488c43c52caefc57213b0d7811ee01ec547568f
Bohinich Serhii <bohinich1@gmail.com>
2025-07-19 15:17:21 +0300

    .

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -7,14 +7,19 @@ dotenv.config();
 export const initMongoDB = async () => {
   try {
     const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGOBD_PASSWORD;
+    const pwd = process.env.MONGODB_PASSWORD; 
     const url = process.env.MONGODB_URL;
     const db = process.env.MONGODB_DB;
+
+    if (!user || !pwd || !url || !db) {
+      throw new Error('One or more MongoDB environment variables are missing');
+    }
+
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+      `mongodb+srv://${user}:${encodeURIComponent(pwd)}@${url}/${db}?retryWrites=true&w=majority`
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
-    throw Error(error.message);
+    throw new Error(error.message);
   }
 };
