22baeacf9ee7ce7ca1fe66327db743bea45604f9
Bohinich Serhii <bohinich1@gmail.com>
2025-08-17 23:01:08 +0300

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,3 @@
-import fs from 'node:fs';
-import { SWAGGER_PATH } from '../constants/index.js'; // шлях до SWAGGER_PATH
-
-try {
-  const data = fs.readFileSync(SWAGGER_PATH, 'utf-8');
-  console.log('Swagger JSON loaded successfully!');
-} catch (err) {
-  console.error('Cannot read swagger.json:', err);
-}
-
+import path from 'node:path';
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
